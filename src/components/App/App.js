13438c32d61a32a3a0f15d284f0e0c255b82b68c
AlDagor <milya89@gmail.com>
2019-12-28 16:35:45 +0200

    add project

---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { App } from './App.styled';
+
+export default () => (
+  <App>
+    <h1>React JS project!</h1>
+  </App>
+);
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const App = styled.div`
+  color: #555;
+  text-align: center;
+`;
